2be353686f5f708ae389c2e5848d7ec815618d70
Dawid Komorowski <dawidkomorowski@outlook.com>
2020-02-03 03:17:02 +0100

    Introduced scene construction script for setting up game level.

---
+++ FlappyBird/FlappyBird/Extension.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Autofac;
+using Geisha.Common.Extensibility;
+using Geisha.Engine.Core.SceneModel;
+
+namespace FlappyBird
+{
+    public sealed class Extension : IExtension
+    {
+        public void Register(ContainerBuilder containerBuilder)
+        {
+            containerBuilder.RegisterType<SetUpLevel>().As<ISceneConstructionScript>().SingleInstance();
+        }
+
+        public string Name { get; } = "Flappy Bird";
+        public string Description { get; } = "";
+        public string Category { get; } = "Game";
+        public string Author { get; } = "Dawid Komorowski";
+        public Version Version { get; } = new Version(0, 1);
+    }
+}
---
+++ FlappyBird/FlappyBird/SetUpLevel.cs
@@ -0,0 +1,14 @@
+﻿using Geisha.Engine.Core.SceneModel;
+
+namespace FlappyBird
+{
+    public sealed class SetUpLevel : ISceneConstructionScript
+    {
+        public string Name { get; } = "SetUpLevel";
+
+        public void Execute(Scene scene)
+        {
+            
+        }
+    }
+}
