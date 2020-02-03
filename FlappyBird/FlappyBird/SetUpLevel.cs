039ede09bfc4e662bef14d12a93fe62c149b2c22
Dawid Komorowski <dawidkomorowski@outlook.com>
2020-02-03 03:26:50 +0100

    Got rectangle on the screen.

--- FlappyBird/FlappyBird/SetUpLevel.cs
+++ FlappyBird/FlappyBird/SetUpLevel.cs
@@ -1,4 +1,8 @@
-﻿using Geisha.Engine.Core.SceneModel;
+﻿using Geisha.Common.Math;
+using Geisha.Engine.Core.Components;
+using Geisha.Engine.Core.SceneModel;
+using Geisha.Engine.Rendering;
+using Geisha.Engine.Rendering.Components;
 
 namespace FlappyBird
 {
@@ -8,7 +12,22 @@ namespace FlappyBird
 
         public void Execute(Scene scene)
         {
-            
+            var camera = new Entity();
+            camera.AddComponent(TransformComponent.Default);
+            camera.AddComponent(new CameraComponent());
+            scene.AddEntity(camera);
+
+            var entity = new Entity();
+            entity.AddComponent(TransformComponent.Default);
+            entity.AddComponent(new RectangleRendererComponent
+            {
+                Color = Color.FromArgb(255, 255, 0, 0),
+                Dimension = new Vector2(400, 300),
+                SortingLayerName = "Default",
+                Visible = true,
+                FillInterior = true
+            });
+            scene.AddEntity(entity);
         }
     }
 }
